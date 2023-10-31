11dedceb85da2d50a9aae62b9c2cfa2d56580085
faelion <alexdlinde@gmail.com>
2023-10-31 05:34:21 +0100

    imgui enabler casi hecho, falta terminar el render y el input

--- smolEditor/smolEditor.cpp
+++ smolEditor/smolEditor.cpp
@@ -78,34 +78,34 @@ int main(int argc, char* argv[])
             engine.camera.up = vec3(0, 1, 0);
 
             // Setup Dear ImGui context
-            IMGUI_CHECKVERSION();
-            ImGui::CreateContext();
-            ImGuiIO& io = ImGui::GetIO(); (void)io;
-            io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-            io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-            io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-            io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+            //IMGUI_CHECKVERSION();
+            //ImGui::CreateContext();
+            //ImGuiIO& io = ImGui::GetIO(); (void)io;
+            //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+            //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+            //io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+            //io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
 
 
-            // Setup Dear ImGui style
+            //// Setup Dear ImGui style
 
-            ImGui::StyleColorsDark();
-            //ImGui::StyleColorsLight();
+            //ImGui::StyleColorsDark();
+            ////ImGui::StyleColorsLight();
 
 
 
-            // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
-            ImGuiStyle& style = ImGui::GetStyle();
-            if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-            {
-                style.WindowRounding = 0.0f;
-                style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-            }
+            //// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+            //ImGuiStyle& style = ImGui::GetStyle();
+            //if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+            //{
+            //    style.WindowRounding = 0.0f;
+            //    style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+            //}
 
 
             // Setup Platform/Renderer backends
-            ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-            ImGui_ImplOpenGL3_Init("#version 130");
+            /*ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+            ImGui_ImplOpenGL3_Init("#version 130");*/
 
 
 
@@ -127,86 +127,86 @@ int main(int argc, char* argv[])
 
 
 
-                // Iniciar el frame de ImGui
-                ImGui_ImplOpenGL3_NewFrame();
-                ImGui_ImplSDL2_NewFrame(window);
-                ImGui::NewFrame();
+                //// Iniciar el frame de ImGui
+                //ImGui_ImplOpenGL3_NewFrame();
+                //ImGui_ImplSDL2_NewFrame(window);
+                //ImGui::NewFrame();
 
                 ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
                 ImGui::DockSpace(dockspace_id, ImVec2(10.0f, 10.0f), ImGuiDockNodeFlags_PassthruCentralNode);
 
 
                 // TEMP: intentando crear la demo window
-                if (showDemoWindow)
-                    ImGui::ShowDemoWindow(&showDemoWindow);
+                /*if (showDemoWindow)
+                    ImGui::ShowDemoWindow(&showDemoWindow);*/
 
-                static float f = 0.0f;
-                static int counter = 0;
+                /*static float f = 0.0f;
+                static int counter = 0;*/
 
-                if (ImGui::Begin("Hello, world!"), ImGuiWindowFlags_MenuBar)
-                {
-                    ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-                    ImGui::Checkbox("Demo Window", &showDemoWindow);      // Edit bools storing our window open/close state
-                    ImGui::Checkbox("Another Window", &showDemoWindow);
-                    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+                //if (ImGui::Begin("Hello, world!"), ImGuiWindowFlags_MenuBar)
+                //{
+                //    ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+                //    ImGui::Checkbox("Demo Window", &showDemoWindow);      // Edit bools storing our window open/close state
+                //    ImGui::Checkbox("Another Window", &showDemoWindow);
+                //    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
 
-                    if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                        counter++;
-                    ImGui::SameLine();
-                    ImGui::Text("counter = %d", counter);
+                //    if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                //        counter++;
+                //    ImGui::SameLine();
+                //    ImGui::Text("counter = %d", counter);
 
-                    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-                    ImGui::End();
-                }
+                //    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+                //    ImGui::End();
+                //}
 
-                // Crear ventanas o paneles que se pueden acoplar
-                if (ImGui::Begin("Panel 1"), ImGuiWindowFlags_MenuBar) {
-                    // Contenido del primer panel
-                    ImGui::Text("Contenido del Panel 1");
-                    ImGui::End();
-                }
+                //// Crear ventanas o paneles que se pueden acoplar
+                //if (ImGui::Begin("Panel 1"), ImGuiWindowFlags_MenuBar) {
+                //    // Contenido del primer panel
+                //    ImGui::Text("Contenido del Panel 1");
+                //    ImGui::End();
+                //}
 
-                if (ImGui::Begin("Panel 2"), ImGuiWindowFlags_MenuBar) {
-                    // Contenido del segundo panel
-                    ImGui::Text("Contenido del Panel 2");
-                    ImGui::End();
-                }
+                //if (ImGui::Begin("Panel 2"), ImGuiWindowFlags_MenuBar) {
+                //    // Contenido del segundo panel
+                //    ImGui::Text("Contenido del Panel 2");
+                //    ImGui::End();
+                //}
 
                 const auto frame_start = steady_clock::now();
                 engine.step(FDT);
                 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
                 engine.render();
-                ImGui::Render();
+                /*ImGui::Render();*/
                 glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
                 glClearColor(bg_color.x * bg_color.w, bg_color.y * bg_color.w, bg_color.z * bg_color.w, bg_color.w);
                 /*glClear(GL_COLOR_BUFFER_BIT);*/
-                ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+                //ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 
 
-                // Update and Render additional Platform Windows
-                // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
-                //  For this specific demo app we could also call SDL_GL_MakeCurrent(window, gl_context) directly)
-                if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-                {
-                    SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
-                    SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
-                    ImGui::UpdatePlatformWindows();
-                    ImGui::RenderPlatformWindowsDefault();
-                    SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
-                }
+                //// Update and Render additional Platform Windows
+                //// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+                ////  For this specific demo app we could also call SDL_GL_MakeCurrent(window, gl_context) directly)
+                //if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+                //{
+                //    SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
+                //    SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
+                //    ImGui::UpdatePlatformWindows();
+                //    ImGui::RenderPlatformWindowsDefault();
+                //    SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
+                //}
 
 
-                SDL_GL_SwapWindow(window);
+                /*SDL_GL_SwapWindow(window);*/
                 const auto frame_end = steady_clock::now();
                 const auto frame_duration = frame_end - frame_start;
                 if (frame_duration < FDT) this_thread::sleep_for(FDT - frame_duration);
             }
         }
 
-        ImGui_ImplOpenGL3_Shutdown();
-        ImGui_ImplSDL2_Shutdown();
-        ImGui::DestroyContext();
+        //ImGui_ImplOpenGL3_Shutdown();
+        //ImGui_ImplSDL2_Shutdown();
+        //ImGui::DestroyContext();
 
         SDL_GL_DeleteContext(gl_context);
 
---
+++ smolEngine/ImguiEnabler.cpp
@@ -0,0 +1,95 @@
+#include "ImguiEnabler.h"
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_opengl3.h"
+#include "Application.h"
+
+ImguiEnabler::ImguiEnabler(bool startEnabled) : Module(startEnabled)
+{
+}
+
+// Destructor
+ImguiEnabler::~ImguiEnabler()
+{
+}
+
+// Called before render is available
+bool ImguiEnabler::Start()
+{
+	//LOG("Init Imgui Enabler");
+	bool ret = true;
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+
+
+	// Setup Dear ImGui style
+
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsLight();
+
+
+
+	// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+	ImGuiStyle& style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
+
+	ImGui_ImplSDL2_InitForOpenGL(app->win->window, gl_context);
+	ImGui_ImplOpenGL3_Init("#version 130");
+
+	return ret;
+}
+
+// Called before quitting
+bool ImguiEnabler::CleanUp()
+{
+	//LOG("Destroying Imgui Enabler");
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+
+	return true;
+}
+
+void ImguiEnabler::BeginUI()
+{
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplSDL2_NewFrame(app->win->window);
+	ImGui::NewFrame();
+	//ImGuizmo::BeginFrame();
+}
+
+void ImguiEnabler::EndUI()
+{
+	ImGuiIO& io = ImGui::GetIO();
+
+	io.DisplaySize = ImVec2((float)app->win->width, (float)app->win->height);
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+	// Update and Render additional Platform Windows
+	// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+	//  For this specific demo app we could also call SDL_GL_MakeCurrent(window, gl_context) directly)
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
+		SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
+		ImGui::UpdatePlatformWindows();
+		ImGui::RenderPlatformWindowsDefault();
+		SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
+	}
+
+	SDL_GL_SwapWindow(app->win->window);
+}
